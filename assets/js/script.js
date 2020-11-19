72d1960ddd19dbc81752fab0610a3b3b982d53b4
subash24 <subash24@users.github.com>
2020-11-19 19:00:40 +0530

    first

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    var _mwi = 991;//width mobile menu enable
+    function toggleDropdown (e) {
+      //min width window 991px - hover menu show 
+      if(window.innerWidth > _mwi) {
+          var _d = $(e.target);
+          setTimeout(function () {
+            var shouldOpen = e.type !== 'click' && _d.is(':hover');
+            _d.parents('.nav-item.dropdown').find('.dropdown-menu').toggleClass('show', shouldOpen);
+            _d.parents('.nav-item.dropdown').toggleClass('show', shouldOpen);
+            $(e.target).attr('aria-expanded', shouldOpen);
+          }, e.type === 'mouseleave' ? 250 : 0);
+       }
+    }
+    
+    $(document)
+        .on('mouseenter','.nav-item.dropdown',toggleDropdown)
+        .on('mouseleave','.nav-item.dropdown.show',toggleDropdown);
+});

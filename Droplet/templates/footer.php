bae2e9cb8316d2c2c25b241e44064adb83026222
subash24 <subash24@users.github.com>
2020-12-25 03:49:00 +0530

    closetofinal

--- Droplet/templates/footer.php
+++
@@ -1,8 +0,0 @@
-
- <footer>
- </footer>
- <script src="vendor/jquery3.5.1.js"></script>
- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.5.4/umd/popper.min.js"></script>
- <script src="vendor/js/bootstrap.js"></script>
- <script src="assets/js/script.js"></script>
-</body>
